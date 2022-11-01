f7c38a03e5f207cb61c395fd926c52806f14dff1
Timothy Pidashev <pidashev.tim@gmail.com>
2022-10-31 22:04:08 -0700

    C++ test; Keystroke_Version prints as hexadecimal

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "version.h"
+#define KEYSTROKE_VERSION(Keystroke_Version)
+
+int main(int, const char **KEYSTROKE_VERSION) {
+    std::cout << "Keystroke Version: " << KEYSTROKE_VERSION << std::endl;
+    return 0;
+}
